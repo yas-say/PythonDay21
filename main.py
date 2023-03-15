89f0ee2e81adf80b08bd85c19dcb1595831c77e5
yas-say <yasowant@live.com>
2023-03-14 22:15:54 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+class Animal:
+    def __init__(self):
+        self.eyes = 2
+
+    def move(self):
+        print("I can walk")
+
+
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+        self.tail = 1
+
+    def swim(self):
+        print("I can swim")
+
+
+nemo = Fish()
+print(nemo.eyes)
+nemo.move()
+print(nemo.tail)
+nemo.swim()
